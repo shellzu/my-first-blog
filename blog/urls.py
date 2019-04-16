71c65ea20e3820c9b8059f858144ce848a9ff5f0
aksh-t <tanno.ao@gmail.com>
2019-04-17 00:56:54 +0900

    formatter

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
          name='add_comment_to_post'),
     path('comment/<int:pk>/approve/',
          views.comment_approve, name='comment_approve'),
-    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/remove/', views.comment_remove,
+         name='comment_remove'),
 ]
